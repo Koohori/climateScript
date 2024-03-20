3170a66de37d70e0124322dd649803dcc76e1469
koohori <ashibesarge2@gmail.com>
2024-03-20 18:51:38 +0900

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+changes_plot=[]
+
+# Constants
+## albedo
+a = 0.3
+#solar constant
+sc = 1367
+# initial temp in kelvin
+ti = 288
+# emissivity
+e = 0.7
+## EarthSurface area
+sa = 5.1*10**14
+## Radius
+r = 6371000
+
+# Surface Capacity
+shc = 4*10**8
+
+## Sigma
+sigma = 5.67*10**-8
+
+
